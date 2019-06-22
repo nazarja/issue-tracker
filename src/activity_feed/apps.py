2caced03855b0bbd27d79e7288bbff97f8fe523a
Sean Murphy <mail.seanmurphy@gmail.com>
2019-06-22 17:49:24 +0100

    Development: Created projects apps structure and inital test run to index template.

---
+++ src/activity_feed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActivityFeedConfig(AppConfig):
+    name = 'activity_feed'
--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -20,6 +20,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # django apps
+    'accounts',
+    'activity_feed',
+    'cart',
+    'charts',
+    'checkout',
+    'comments',
+    'pages',
+    'tickets',
 ]
 
 MIDDLEWARE = [
@@ -96,5 +106,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
