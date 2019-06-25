ceb9ef40766c6762268ab0b7dad8afdb5e717351
Sean Murphy <mail.seanmurphy@gmail.com>
2019-06-25 13:58:48 +0100

    Removed: Deleted redundent django app: pages, reason = unnecessary

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'charts',
     'checkout',
     'comments',
-    'pages',
     'tickets',
 ]
 
--- src/project/urls.py
+++ src/project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='pages/index.html'), name='index'),
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
 ]
 
