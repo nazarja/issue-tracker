30d07399c4c3bfd4ee640720bec40e57607600d7
Sean Murphy <mail.seanmurphy@gmail.com>
2019-07-05 15:03:53 +0100

    Add: Add validatioon for CVV and Credit Card numbers must be digits only.

--- src/checkout/forms.py
+++ src/checkout/forms.py
@@ -6,15 +6,32 @@ YEAR_CHOICES = [(i, i) for i in range(2019, 2036)]
 
 
 class OrderForm(forms.ModelForm):
+    street_address1 = forms.CharField(label="Street Address 1", max_length=100, required=True)
+    street_address2 = forms.CharField(label="Street Address 2", max_length=100, required=False)
+
     class Meta:
         model = Order
         fields = '__all__'
+        exclude = ('date',)
 
 
 class PaymentForm(forms.Form):
-    credit_card_number = forms.CharField(label='Credit card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
+    credit_card_number = forms.CharField(max_length=19, label='Credit card number', required=True)
+    cvv = forms.CharField(max_length=3, label='Security code (CVV)', required=True)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
+    def clean_credit_card_number(self):
+        data = self.cleaned_data['cvv']
+
+        if not data.isdigit():
+            raise forms.ValidationError("The credit card number must only contain numbers")
+        return data
+
+    def clean_cvv(self):
+        data = self.cleaned_data['cvv']
+
+        if not data.isdigit():
+            raise forms.ValidationError("The CVV must only contain numbers")
+        return data
--- src/checkout/models.py
+++ src/checkout/models.py
@@ -4,14 +4,13 @@ from django.utils import timezone
 
 
 class Order(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
+    full_name = models.CharField(max_length=80, blank=False)
+    street_address1 = models.CharField(max_length=100, blank=False)
+    street_address2 = models.CharField(max_length=100, blank=True)
+    town_or_city = models.CharField(max_length=60, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
+    country = models.CharField(max_length=40, blank=False)
     date = models.DateField(default=timezone.now)
 
     def __str__(self):
