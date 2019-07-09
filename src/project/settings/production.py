bd53da31da134e6e8ab5b411bb37342990b288c9
Sean Murphy <mail.seanmurphy@gmail.com>
2019-07-09 02:46:09 +0100

    Turning on debug in development to figure out strange issue when creating a feature.

--- src/project/settings/production.py
+++ src/project/settings/production.py
@@ -8,7 +8,7 @@ import dj_database_url
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allowed Hosts
 ALLOWED_HOSTS = [
